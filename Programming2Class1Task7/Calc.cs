e53fde358e3eacd9cc7427255254dc256b826a05
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2021-07-20 09:37:28 +1200

    Add comments describing class and methods

--- Programming2Class1Task7/Calc.cs
+++ Programming2Class1Task7/Calc.cs
@@ -6,39 +6,71 @@ using System.Threading.Tasks;
 
 namespace Programming2Class1Task7
 {
+    // Class containing the calculator functionality
     class Calc
     {
         // Always store previous input.
         decimal previous;
-        public decimal Add()
+
+        /* 
+         * Use: 
+         * Function: 
+         * Parameters: 
+         * Return: 
+         */
+        public decimal Add(decimal input1, decimal input2)
         {
             decimal result = 0;
 
             return result;
         }
 
-        public decimal Sub()
+        /* 
+         * Use: 
+         * Function: 
+         * Parameters: 
+         * Return: 
+         */
+        public decimal Sub(decimal input1, decimal input2)
         {
             decimal result = 0;
 
             return result;
         }
 
-        public decimal Mul()
+        /* 
+         * Use: 
+         * Function: 
+         * Parameters: 
+         * Return: 
+         */
+        public decimal Mul(decimal multiplicand, decimal multiplier)
         {
             decimal result = 0;
 
             return result;
         }
 
-        public decimal Div()
+        /* 
+         * Use: 
+         * Function: 
+         * Parameters: 
+         * Return: 
+         */
+        public decimal Div(decimal dividend, decimal divisor)
         {
             decimal result = 0;
 
             return result;
         }
 
-        public decimal Mod()
+        /* 
+         * Use: 
+         * Function: 
+         * Parameters: 
+         * Return: 
+         */
+        public decimal Mod(decimal n, int mod)
         {
             decimal result = 0;
 
