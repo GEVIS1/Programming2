061bb59fa332bf8c6a8050a30b80c9e633796127
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2021-07-20 09:28:23 +1200

    Add Calc class with skeleton methods

---
+++ Programming2Class1Task7/Calc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming2Class1Task7
+{
+    class Calc
+    {
+        // Always store previous input.
+        decimal previous;
+        public decimal Add()
+        {
+            decimal result = 0;
+
+            return result;
+        }
+
+        public decimal Sub()
+        {
+            decimal result = 0;
+
+            return result;
+        }
+
+        public decimal Mul()
+        {
+            decimal result = 0;
+
+            return result;
+        }
+
+        public decimal Div()
+        {
+            decimal result = 0;
+
+            return result;
+        }
+
+        public decimal Mod()
+        {
+            decimal result = 0;
+
+            return result;
+        }
+
+    }
+}
